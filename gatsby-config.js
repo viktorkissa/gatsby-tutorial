f12511d42ca2699a2ff4b10c8ead4f93411119ff
viktorkissa <vkibank@gmail.com>
2020-10-11 17:32:07 +0300

    sourceInstanceName and gatsbyImage

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,20 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
         path: `${__dirname}/src/images/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts/`
       }
     }
   ],
