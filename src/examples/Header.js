cc6e3552cd7ddf716139c6c97d5cde28f98f0c66
viktorkissa <vkibank@gmail.com>
2020-10-11 16:02:23 +0300

    Query keyword and name

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -4,7 +4,7 @@ import { useStaticQuery, graphql } from "gatsby";
 import Layout from '../components/layout';
 
 const getSiteMetaData = graphql`
-    {
+    query FirstQuery {
         site {
             info: siteMetadata {
                 person {
