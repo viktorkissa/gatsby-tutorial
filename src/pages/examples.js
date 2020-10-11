2c7fe92d30065cdabf5f7e9641405492ad07ab90
viktorkissa <vkibank@gmail.com>
2020-10-11 16:21:51 +0300

    PageQuery

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { graphql } from "gatsby";
 
 import Header from '../examples/Header';
 import HeaderStatic from "../examples/HeaderStatic";
 
-export default () =>
-  <div>
-    <h1>Examples Page</h1>
-    <Header />
-    <HeaderStatic />
-  </div>
+export default ({data}) => {
+  const {
+    site: {
+      info: {
+        author
+      }
+    }
+  } = data;
+
+  return (
+    <>
+      <h1>Examples Page - {author}</h1>
+      <Header />
+      <HeaderStatic />
+    </>
+  )
+};
+
+export const data = graphql`
+    {
+        site {
+            info: siteMetadata {
+                person {
+                    age
+                    name
+                }
+                author
+                data
+                description
+                title
+            }
+        }
+    }
+`;
